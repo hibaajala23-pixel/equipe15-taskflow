001ed9b63c33ac9a583743dc3a029404fa29e8b3
zak <hayat_zak@um5.ac.ma>
2026-05-30 03:10:53 +0200

    changement des titres

--- reset_password.php
+++ reset_password.php
@@ -113,7 +113,7 @@ try {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Reset Password</title>
+<title>TaskFlow - Reset Password</title>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
